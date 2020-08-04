591a2b6dbaa3ff2fe47aa8441b226dbc35346a99
Andrey <akinava@gmail.com>
2020-08-04 16:00:33 +0200

    tested on Ubuntu 20.04

--- ecr_pass.sh
+++ ecr_pass.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+# required tools:
+# curl sed openssl base64 date
+
+
 key=$AWS_ACCESS_KEY_ID
 secret=$AWS_SECRET_ACCESS_KEY
 
@@ -31,18 +35,18 @@ day_now=$(date -u +%Y%m%d)
 
 
 data="{}"
-data_hash=$(echo -n $data | openssl dgst -sha256)
+data_hash=$(echo -n $data | openssl dgst -sha256 | sed "s/^.* //")
 cr="POST\n/\n\ncontent-type:application/x-amz-json-1.1\nhost:api.ecr.${region}.amazonaws.com\nx-amz-date:${time_now}\nx-amz-target:AmazonEC2ContainerRegistry_V20150921.GetAuthorizationToken\n\ncontent-type;host;x-amz-date;x-amz-target\n${data_hash}"
-cr_hash=$(echo -ne $cr | openssl dgst -sha256)
+cr_hash=$(echo -ne $cr | openssl dgst -sha256 | sed "s/^.* //")
 string_to_sign="AWS4-HMAC-SHA256\n${time_now}\n${day_now}/${region}/ecr/aws4_request\n${cr_hash}"
-k_date=$(echo -n $day_now | openssl dgst -sha256 -hmac "AWS4${secret}")
-k_region=$(echo -n $region | openssl dgst -sha256 -mac HMAC -macopt hexkey:${k_date})
-k_service=$(echo -n "ecr" | openssl dgst -sha256 -mac HMAC -macopt hexkey:${k_region})
-k_signing=$(echo -n "aws4_request" | openssl dgst -sha256 -mac HMAC -macopt hexkey:${k_service})
-signature=$(echo -ne $string_to_sign | openssl dgst -sha256 -mac HMAC -macopt hexkey:${k_signing})
+k_date=$(echo -n $day_now | openssl dgst -sha256 -hmac "AWS4${secret}" | sed "s/^.* //")
+k_region=$(echo -n $region | openssl dgst -sha256 -mac HMAC -macopt hexkey:${k_date} | sed "s/^.* //")
+k_service=$(echo -n "ecr" | openssl dgst -sha256 -mac HMAC -macopt hexkey:${k_region} | sed "s/^.* //")
+k_signing=$(echo -n "aws4_request" | openssl dgst -sha256 -mac HMAC -macopt hexkey:${k_service} | sed "s/^.* //")
+signature=$(echo -ne $string_to_sign | openssl dgst -sha256 -mac HMAC -macopt hexkey:${k_signing} | sed "s/^.* //")
 
 
-responce=$(curl -s --data "{}" \
+responce=$(curl -s --data ${data} \
     -H "X-Amz-Target: AmazonEC2ContainerRegistry_V20150921.GetAuthorizationToken" \
     -H "Content-Type: application/x-amz-json-1.1" \
     -H "X-Amz-Date: ${time_now}" \
